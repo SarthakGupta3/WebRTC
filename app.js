9f12a8a7df0cf02520b13f80209b6c37a994717d
SArthak3661 <guptasarthak451@gmail.com>
2019-10-08 17:30:57 +0530

    Initial commit

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const socket = require('socket.io');
 const app = express();
 app.use(express.static('public'));
-app.get('/',(req,res,next) => {
-    res.send('hi');
-})
-app.get('/chat',(req,res,next) => {
+app.get('/', (req,res,next) => {
     res.sendFile(path.join(__dirname, 'public', 'rtc.html'));
+});
+
+const server = app.listen(3000);
+
+const io = socket(server);
+
+io.on('connection', function(socket){
+    socket.on('video-offer', function(data){
+        io.emit('video-offer', data);
+    })
+    socket.on('video-answer', function(data){
+       io.emit('video-answer', data);
+    })
+    socket.on('newICE',function(data){
+        io.emit('addICECandidate', data);
+    })
 })
 
-app.listen(process.env.PORT || 3000);
